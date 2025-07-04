ff8b4d4240dbbd8ae2f2de231981c03581d889e2
bkarogyam <bkarogyam@bkarogyam.bkarogyam>
2025-07-05 00:11:09 +0530

    app updated

--- app/home/whatsapp.jsx
+++ app/home/whatsapp.jsx
@@ -7,7 +7,7 @@ const FloatingButtons = () => {
     return (
         <div className="fixed bottom-5 left-0 w-full flex justify-between px-5 z-[1000]">
             <motion.a
-                href="tel:+9183685661339"
+                href="tel:+918368661339"
                 className="w-[50px] h-[50px] bg-blue-700 text-white rounded-full shadow flex items-center justify-center"
                 initial={{ y: 0 }}
                 animate={{ y: [0, -10, 0] }}
@@ -17,7 +17,7 @@ const FloatingButtons = () => {
             </motion.a>
 
             <motion.a
-                href="https://wa.me/83685661339"
+                href="https://wa.me/8368661339"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="w-[50px] h-[50px] bg-[#25D366] text-white rounded-full shadow flex items-center justify-center"
